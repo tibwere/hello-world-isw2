6c64d1f8fb0e9859a14ce218f1dec92f31d3c83f
Simone Tiberi <tib.sim.98@gmail.com>
2021-03-07 00:02:55 +0100

    #4

---
+++ src/main/java/it/uniroma2/isw2/hello_world_isw2/App.java
@@ -0,0 +1,12 @@
+package it.uniroma2.isw2.hello_world_isw2;
+
+import java.util.logging.Logger;
+
+public class App {
+	
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(App.class.getName());
+		logger.info("Hello World");
+	}
+	
+}
---
+++ src/test/java/it/uniroma2/isw2/hello_world_isw2/AppTest.java
@@ -0,0 +1,20 @@
+package it.uniroma2.isw2.hello_world_isw2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
