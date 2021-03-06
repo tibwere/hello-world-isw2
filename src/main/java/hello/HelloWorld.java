6c3793d737ee69f6f7acb25b1465e8c5e6ee268f
Simone Tiberi <tib.sim.98@gmail.com>
2021-03-06 14:44:08 +0100

    #1

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(HelloWorld.class.getName());
+		logger.info("Hello World");
+	}
+
+}
